0563c47c6cc53087e96b381ebeb124e81bb847cd
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2022-11-22 13:27:10 +0700

    feat: add main, style: tidy up dadu

--- src/dadu.pl
+++ src/dadu.pl
@@ -10,15 +10,16 @@ dice2(0).
 sameDiceNum(0).
 
 /* prosedur throwDice */
-throwDice :- random(1,7,X),
-            random(1,7,Y),
-            retractall(dice1(_)),
-            retractall(dice2(_)),
-            asserta(dice1(X)),
-            asserta(dice2(Y)),
-            write('Dice 1: '), write(X), nl,
-            write('Dice 2: '), write(Y), nl,
-            write('Anda maju sebanyak '), Z is X+Y, write(Z), write(' langkah.'), nl,
-            (X == Y, write('Double!')).
+throwDice :-
+    random(1,7,X),
+    random(1,7,Y),
+    retractall(dice1(_)),
+    retractall(dice2(_)),
+    asserta(dice1(X)),
+    asserta(dice2(Y)),
+    write('Dice 1: '), write(X), nl,
+    write('Dice 2: '), write(Y), nl,
+    write('Anda maju sebanyak '), Z is X+Y, write(Z), write(' langkah.'), nl,
+    (X == Y, write('Double!')).
 
 sameDice :- dice1(A), dice2(B), A == B, sameDiceNum(X), Y is X+1, retractall(sameDiceNum(_)), asserta(sameDiceNum(Y)).
---
+++ src/main.pl
@@ -0,0 +1,35 @@
+/* Include file */
+:- include('chance_card.pl').
+:- include('dadu.pl').
+:- include('jail.pl').
+:- include('player.pl').
+:- include('property.pl').
+:- include('Loc.pl').
+
+/* Status permainan */
+:- dynamic(isExit/1).
+isExit(1).
+nowPlayer(v).
+
+start :-
+    retractall(isExit(1)), asserta(isExit(0)), retractall(nowPlayer(_)), asserta(nowPlayer(v)),
+    write('Misalnya ini pesan pembuka xixixixi'), nl,
+    write('Sekarang adalah giliran pemain '),
+    nowPlayer(X), player(X,Nama,_,_,_,_,_,_,_),
+    write(Nama), write('.'), nl,
+    write('Ketik map untuk melihat map.').
+
+/* map sudah diimplementasikan di file ... */
+/* throwDice, dice1, dice2 sudah diimplementasikan di file ... */
+
+
+/* bankrupt */
+bankrupt(v) :-
+    retractall(player(v,_,_,_,_,_,_,_,_)),
+    assertz(player(v,'V',go,20000,0,0,[],[],[])).
+
+bankrupt(w) :-
+    retractall(player(w,_,_,_,_,_,_,_,_)),
+    assertz(player(w,'W',go,20000,0,0,[],[],[])).
+
+/* bentar ya belum selesai hehehe */
